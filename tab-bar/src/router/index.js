e2d5054e4e4f98fe6b4672e79e7eb54345c91397
cgy <593056561@qq.com>
2020-04-29 22:59:17 +0800

    demo

--- tab-bar/src/router/index.js
+++ tab-bar/src/router/index.js
@@ -1,22 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-
+import User from '../views/User.vue'
 Vue.use(VueRouter)
 
-  const routes = [
+const routes = [
   {
-    path: '/',
+    path: '/home',
     name: 'Home',
     component: Home
   },
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import('../views/About.vue')
+  },
+  {
+    path: '/user/:id',
+    name: 'User',
+    component: User,
   }
 ]
 
---
+++ tab-bar/src/views/User.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>用户</p>
+    <p>{{id}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      a: 123
+    };
+  },
+  computed: {
+    id() {
+      return this.$route.params.id;
+    }
+  }
+};
+</script>
